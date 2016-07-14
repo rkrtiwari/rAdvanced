9ff1142429d5874dd2354c4eb322a46fae4694aa
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2016-07-14 19:36:01 +0800

    made modification to model selection and regularization

--- LinearModelSelectionAndRegularization.R
+++ LinearModelSelectionAndRegularization.R
@@ -1,43 +1,43 @@
 #install.packages("leaps")
 
 ## Forward selection model
+##install.packages("leaps")
+
 library(leaps)
 fwdSelection <- regsubsets(mpg ~ ., data = mtcars, method = "forward")
 sumFwdSel <- summary(fwdSelection)
 names(sumFwdSel)
 
 sumFwdSel$outmat 
-sumFwdSel$which[ind,]
-
 ind <- which.max(sumFwdSel$adjr2)
 ind
+sumFwdSel$which[ind,]
 coef(fwdSelection, ind)
 
 ## Lasso model
+## install.packages("glmnet")
+
 library(glmnet)
 x <- model.matrix(mpg ~ ., mtcars)[,-1] 
 y <- mtcars$mpg
-
-grid <- 10^seq(10,-2, length = 100)  # lambda values
-
-lassoMod <- glmnet(x, y, alpha = 1,lambda = grid) # alpha = 0 (ridge)
-dim(coef(lassoMod)) 
-
 
 set.seed(1) 
 train <- sample(1: nrow(x), nrow(x)/2) 
 test <- (-train) 
 yTest <- y[test]
 
-cv.out <- cv.glmnet(x[train,],y[train], alpha=1, nfolds = 5) 
-plot(cv.out) 
+cv.out <- cv.glmnet(x[train,],y[train], alpha=1, nfolds = 5)   # alpha = 0 (lasso)
 bestlam  <- cv.out$lambda.min 
 lasso.pred <- predict(cv.out ,s=bestlam ,newx=x[test,]) 
-mean((lasso.pred -yTest)^2) 
+mean((lasso.pred - yTest)^2) 
 
+bestlam
 
+lasso.pred <- predict(cv.out ,s=5 ,newx=x[test,]) 
+mean((lasso.pred - yTest)^2)
 
-
+lasso.pred <- predict(cv.out ,s=0.1 ,newx=x[test,]) 
+mean((lasso.pred - yTest)^2)
 
 
 
