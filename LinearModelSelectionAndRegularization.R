510ca828fb2e87b2a7d5bbf9f8485acef83ab0d8
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2016-08-24 11:52:02 +0800

    Modified regularization part

--- LinearModelSelectionAndRegularization.R
+++ LinearModelSelectionAndRegularization.R
@@ -17,27 +17,42 @@ coef(fwdSelection, ind)
 ## Lasso model
 ## install.packages("glmnet")
 
+## Lasso model
+## install.packages("glmnet")
+
 library(glmnet)
+
+## 1. Creating training and the test data set
 x <- model.matrix(mpg ~ ., mtcars)[,-1] 
 y <- mtcars$mpg
 
 set.seed(1) 
-train <- sample(1: nrow(x), nrow(x)/2) 
-test <- (-train) 
-yTest <- y[test]
+inTrain <- sample(1: nrow(x), 0.6*nrow(x)) 
+yTest <- y[-inTrain]
 
-cv.out <- cv.glmnet(x[train,],y[train], alpha=1, nfolds = 5)   # alpha = 0 (lasso)
-bestlam  <- cv.out$lambda.min 
-lasso.pred <- predict(cv.out ,s=bestlam ,newx=x[test,]) 
-mean((lasso.pred - yTest)^2) 
+## 2. Model training
+lModel <- glmnet(x[inTrain,],y[inTrain], alpha=1) # alpha = 0 (ridge)
 
-bestlam
+## 3. Model Prediction
+predV <- predict(lModel, newx = x[-inTrain,], s = 2)
 
-lasso.pred <- predict(cv.out ,s=5 ,newx=x[test,]) 
-mean((lasso.pred - yTest)^2)
+## 4. Model visualization
+plot(lModel, xvar = "lambda", label = TRUE, lwd =2, 
+     cex.axis = 1.5, cex.lab = 1.5)
+coef(lModel, s = exp(-1))
+coef(lModel, s = exp(-1))[5]
 
-lasso.pred <- predict(cv.out ,s=0.1 ,newx=x[test,]) 
-mean((lasso.pred - yTest)^2)
+## 5. Model Assesment
+mean((predV - yTest)^2)
+
+## 6. Finding the optimal value of lambda using cross-validation
+cvModel <- cv.glmnet(x[train,],y[train], alpha=1, nfolds = 5) 
+bestlam  <- cvModel$lambda.min 
+cvpredV <- predict(cvModel, s=bestlam, newx=x[-inTrain,]) 
+mean((cvpredV - yTest)^2) 
+
+## 7. Variation of mean-squared error with the penalty parameter
+plot(cvModel)
 
 
 
